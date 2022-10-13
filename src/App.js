78ff5e29580fb192acaac133d0778d46a6ef0e91
kumkum <kumkumrathour00@gmail.com>
2022-10-13 20:39:41 +0530

    push the data

--- src/App.js
+++ src/App.js
@@ -1,34 +1,80 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
+import AddMovie from './components/AddMovie';
 import './App.css';
 
 function App() {
-   const [car , setCar] = useState([])
-  function fetchCarHandler(){
-     fetch('https://swapi.dev/api/starships/').then((response)=>{
-      return response.json();
-     }).then((data)=>{
-      const transformedCar = data.results.map((carData) =>{
-        return {
-          id:carData.model,
-          title:carData.name,
-          openingText: carData.manufacturer,
-          releaseData:carData.length
-        }
-      })
-          setCar(transformedCar)
-     })
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchMoviesHandler = useCallback(async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch('https://apigetrequest-default-rtdb.firebaseio.com/movies.json');
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+
+      const data = await response.json();
+      const loadedMovies = [];
+
+      for(const key in data){
+        loadedMovies.push({
+          id: key ,
+          title:data[key].title,
+          openingText: data[key].openingText,
+          releaseDate:data[key].releaseDate
+        })
+      }
+
+      setMovies(loadedMovies);
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+
+   async function addMovieHandler(movie) {
+    const response = await fetch('https://apigetrequest-default-rtdb.firebaseio.com/movies.json',{
+    method: 'POST',
+    body: JSON.stringify(movie),
+        headers:{
+          'Content-Type' : 'application/json'
+        }} );
+        const data = await response.json();
+        console.log(data)
+      }
+
+  let content = <p>Found no movies.</p>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
   }
 
   return (
     <React.Fragment>
       <section>
-        <button onClick={fetchCarHandler}> Fetch car data</button>
+        <AddMovie onAddMovie={addMovieHandler} />
       </section>
       <section>
-        <MoviesList movies={car} />
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
